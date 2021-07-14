5106ea0222fa37df9581e20465c46dffdb1037ea
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-07-13 22:57:08 -0400

    Create model for Project

--- bugs/models.py
+++ bugs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields import CharField, TextField
 
-# Create your models here.
+class Project(models.Model):
+    """Define project models."""
+    title = models.CharField(max_length=150)
+    details = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.details
