9fa91f05494f3878a06746d5cbe0adaf244d6532
Siv Sky <6752411+spolanco94@users.noreply.github.com>
2021-07-15 00:13:31 -0400

    Create a class defining the Ticket Model
    The Ticket class will be linked to the Project model with a many-to-one
    relationship via ForeignKey.
    
    This class will consist of a Title CharField section limited to 150
    max characters, a Details text field, a Priority ChoiceField, a
    DateTimeField that will automatically add the Date and Time from
    creation, and a file upload FileFiled where users may add any optional
    attachments, with support for multiple files at once.

--- bugs/models.py
+++ bugs/models.py
@@ -1,11 +1,35 @@
 from django.db import models
 from django.db.models.fields import CharField, TextField
+from django.db.models.fields.related import ForeignKey
 
 class Project(models.Model):
-    """Define project models."""
+    """A project currently being tracked."""
     title = models.CharField(max_length=150)
     details = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.title
+
+class Ticket(models.Model):
+    """Define ticket specific to a project."""
+    PRIORITY_LEVEL_CHOICES = [
+        ('H', 'High'),
+        ('M', 'Medium'),
+        ('L', 'Low'),
+    ]
+    
+    project = ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    details = models.TextField()
+    priority_level = models.CharField(
+        max_length=1, 
+        choices=PRIORITY_LEVEL_CHOICES,
+        default='M',
+    )
+    date_added = models.DateTimeField(auto_now_add=True)
+    attachments = models.FileField(
+        upload_to='attachments/', 
+        blank=True, 
+        null=True
+    )
