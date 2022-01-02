a53ee15ea5af3ff06398f633dc7a59be7f130559
Steven Polanco <6752411+spolanco94@users.noreply.github.com>
2022-01-02 01:45:18 -0500

    Implement Django's password reset function
    Used Django's default password reset functions to allow users to reset their
    passwords via email link. Production will use an email service, but for now
    emails are stored locally as a .txt file. Inlcuded custom templates and updated
    success_url to use 'users' namespace.

--- bugger/settings.py
+++ bugger/settings.py
@@ -135,3 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 LOGIN_URL = 'users:login'
 AUTH_USER_MODEL = 'users.User'
+
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,29 @@
 from django import urls
 from django.urls import path
 from django.urls.conf import include
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 from . import views
 
 app_name = 'users'
 urlpatterns = [
-    # Login page
-    path('', include('django.contrib.auth.urls')),
     # Registration page
     path('register/', views.register, name='register'),
+    # Password Reset
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        success_url=reverse_lazy('users:password_reset_done')
+    ), name='password_reset'),
+    # Password Reset Done
+    path('password_reset/', auth_views.PasswordResetDoneView.as_view(), 
+        name='password_reset_done'),
+    # Password Reset Confirm
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('users:password_reset_complete')
+    ), name='password_reset_confirm'),
+    # Password Reset Complete
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), 
+        name='password_reset_complete'),
+    # Default authentication URL's
+    path('', include('django.contrib.auth.urls')),
 ]
